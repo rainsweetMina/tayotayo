abe1b9e88f89e355d25a3ba18e4c9254a2118688
heeha153 <heeha153@gmail.com>
2025-03-20 23:21:32 +0900

    기본 틀 작성 중

--- src/main/java/kroryi/bus2/controller/BusController.java
+++ src/main/java/kroryi/bus2/controller/BusController.java
@@ -1,13 +1,10 @@
 package kroryi.bus2.controller;
 
-import kroryi.bus2.repository.BusStopRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -15,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class BusController {
 
     @GetMapping("/bus")
-    public String getBusStops(Model model) {
-        return "busStops";
+    public String getBusStops() {
+        return "bus/busStops";
     }
 }
