2f2472055f40c8d9378ae5420f70a02903159b5f
rainsweetMina <rainsweeets@gmail.com>
2025-03-24 16:44:43 +0900

    3/24 최최종

---
+++ src/main/java/kroryi/bus2/service/BusApiService.java
@@ -0,0 +1,25 @@
+package kroryi.bus2.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class BusApiService {
+
+    private final WebClient webClient;
+
+
+    public String getBusArrivalInfo(String busStopId) {
+        String BASE_URL = "https://apis.data.go.kr/6270000/dbmsapi01";
+        String API_KEY = "oDPMcPKGx7dsFyVw5YzReqSK07UuJoUrABe2dbwM7zt9yVfOjSlE7SQtdIir+EW+DWAcIvio0lm1rR2sMnW7iw==";
+        return webClient.get()
+                .uri(BASE_URL + "/arrival?busStopId=" + busStopId + "&serviceKey=" + API_KEY)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+
+}
--- src/main/java/kroryi/bus2/service/BusDataService.java
+++ src/main/java/kroryi/bus2/service/BusDataService.java
@@ -16,11 +16,14 @@ import kroryi.bus2.repository.RouteRepository;
 import kroryi.bus2.util.FakeRedis;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +38,45 @@ public class BusDataService {
     private final ObjectMapper objectMapper;
     private final FakeRedis fakeRedis;
 
+
+    //    Redis 설정
+    private final BusApiService busApiService;
+    private final long CACHE_EXPIRATION = 60;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    //   Redis 관련
+    public String getBusArrival(String busStopId) {
+        // Redis에서 캐싱된 데이터 가져오기
+        String key = "busArrival:" + busStopId;
+        String cachedData = (String) redisTemplate.opsForValue().get(key);
+
+        if (cachedData != null) {
+            System.out.println("Redis에서 데이터 가져옴");
+            return cachedData;
+        }
+
+        // Redis에 캐싱된 데이터가 없으면 API 에서 가져오기
+        System.out.println("API에서 데이터 가져옴");
+        String response = busApiService.getBusArrivalInfo(busStopId);
+
+        // Redis에 TTL 60초로 저장 (자동 갱신X)
+        redisTemplate.opsForValue().set(key, response, 60, TimeUnit.SECONDS);
+
+        return response;
+
+    }
+
+    public void loadBusStopsToRedis() {
+        List<BusStop> busStops = busStopRepository.findAll();
+        for (BusStop stop : busStops) {
+            // Redis에 값 저장
+            redisTemplate.opsForValue().set("bus_stop:" + stop.getId(), stop);
+        }
+    }
+
+//    Redis 설정 끝
 
     public JsonNode getBusStopNav(String apiUrl) {
 
