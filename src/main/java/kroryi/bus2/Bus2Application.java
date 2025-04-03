18c780956d6f1bf3a9ba86262b570133143b93e0
leeoon-p <sangryun21@gmail.com>
2025-04-03 12:42:34 +0900

    04.03 RouteStopLink

--- src/main/java/kroryi/bus2/Bus2Application.java
+++ src/main/java/kroryi/bus2/Bus2Application.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableJpaRepositories(basePackages = {
@@ -21,7 +20,6 @@ public class Bus2Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Bus2Application.class, args);
     }
-
 
     // Redis 저장 확인용 로그
     @Override
