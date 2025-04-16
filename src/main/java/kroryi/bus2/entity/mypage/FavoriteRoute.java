3365c37a365da8e49935582f1403478c5520f2bc
kyb4218 <kyb4218@gmail.com>
2025-04-16 12:00:28 +0900

    0416

---
+++ src/main/java/kroryi/bus2/entity/mypage/FavoriteRoute.java
@@ -0,0 +1,34 @@
+package kroryi.bus2.entity.mypage;
+
+import jakarta.persistence.*;
+import kroryi.bus2.entity.Route;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import kroryi.bus2.entity.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteRoute {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id")
+    private Route route;    // 노선 ID
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;  // 해당 즐겨찾기를 한 사용자
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;  // 생성일시
+}
