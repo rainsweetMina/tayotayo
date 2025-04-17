464f3a7730217157831b13606301795d86e25ffe
leeoon-p <sangryun21@gmail.com>
2025-04-17 11:54:45 +0900

    Refactor 컨트롤러 리팩토링

--- src/main/java/kroryi/bus2/controller/board/BusFareApiController.java
+++ src/main/java/kroryi/bus2/controller/board/BusFareApiController.java
@@ -3,53 +3,39 @@ package kroryi.bus2.controller.board;
 import io.swagger.v3.oas.annotations.Operation;
 import kroryi.bus2.dto.board.BusFareDTO;
 import kroryi.bus2.entity.BusFare;
-import kroryi.bus2.repository.jpa.board.BusFareRepository;
 import kroryi.bus2.service.board.BusFareService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/fares")
 @RequiredArgsConstructor
-public class BusFareController {
-    private final BusFareRepository busFareRepository;
+public class BusFareApiController {
     private final BusFareService busFareService;
 
-    @GetMapping("/fare")
-    public String showBusFare(Model model) {
-        List<BusFare> fareList = busFareRepository.findAll();
-        model.addAttribute("fares", fareList);
-        return "/board/busFare";
-    }
-
-    @GetMapping("/api/fares")
-    @ResponseBody
+    @GetMapping
     @Operation(summary = "버스 요금 전체 조회", description = "모든 버스 요금 정보를 조회합니다.")
     public List<BusFare> getFares() {
         return busFareService.findAll();
     }
 
-    @PostMapping("/api/fares")
-    @ResponseBody
+    @PostMapping
     @Operation(summary = "버스 요금 등록", description = "새로운 버스 요금 정보를 등록합니다. /" +
             " busType = G(일반), E(급행), D(직행) / payType = card, cash")
     public ResponseEntity<BusFare> createFare(@RequestBody BusFareDTO dto) {
         return ResponseEntity.ok(busFareService.save(dto));
     }
 
-    @PutMapping("/api/fares/{id}")
-    @ResponseBody
+    @PutMapping("/{id}")
     @Operation(summary = "버스 요금 수정", description = "기존 버스 요금 정보를 수정합니다.")
     public ResponseEntity<BusFare> updateFare(@PathVariable int id, @RequestBody BusFareDTO dto) {
         return ResponseEntity.ok(busFareService.update(id, dto));
     }
 
-    @DeleteMapping("/api/fares/{id}")
-    @ResponseBody
+    @DeleteMapping("/{id}")
     @Operation(summary = "버스 요금 삭제", description = "ID로 해당 버스 요금 정보를 삭제합니다.")
     public ResponseEntity<String> deleteFare(@PathVariable int id) {
         busFareService.deleteById(id);
