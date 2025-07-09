2ffce6869fe07e083e2154795dbb9fa260f56b77
plusmail <plus4957@gmail.com>
2025-07-10 04:11:34 +0900

    dddd

--- src/main/java/kroryi/bus2/config/security/SecurityConfig.java
+++ src/main/java/kroryi/bus2/config/security/SecurityConfig.java
@@ -155,6 +155,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/qna/page", "/api/qna/{id}").permitAll()
                         .requestMatchers("/api/found/**").permitAll()  // 습득물 관련 API 공개 접근 허용
                         .requestMatchers("/api/lost/**").permitAll()  // 분실물 관련 API 공개 접근 허용
+                        .requestMatchers("/api/schedule/**").permitAll()  // 버스 시간표 API 공개 접근 허용
                         .requestMatchers("/api/companies", "/api/route-nos-low", "/api/route-notes", "/api/route-id/**", "/api/schedule-header", "/api/route-map", "/api/lowbus-scheduls").permitAll()  // 버스 관련 API 공개 접근 허용
 
                         /* USER·ADMIN 공용(로그인 필요) */
