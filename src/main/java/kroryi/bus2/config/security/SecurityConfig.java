e241e4c91d82934a1d4aaf869bb59d50e05aba61
plusmail <plus4957@gmail.com>
2025-07-11 11:17:17 +0900

    ddddddsssdddsd

--- src/main/java/kroryi/bus2/config/security/SecurityConfig.java
+++ src/main/java/kroryi/bus2/config/security/SecurityConfig.java
@@ -92,7 +92,14 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(swaggerAuthFilter,  UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(apiKeyAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(form -> form.disable()) // 폼 로그인 완전 비활성화
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .successHandler(customLoginSuccessHandler)
+                        .failureHandler((request, response, ex) -> { /* 생략 */ })
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/auth/logout")
                         .logoutSuccessHandler(customLogoutSuccessHandler)
