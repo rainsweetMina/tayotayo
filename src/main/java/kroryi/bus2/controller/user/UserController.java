4379dee33becc0d28495e29b69a167223053359c
plusmail <plus4957@gmail.com>
2025-07-25 16:03:47 +0900

    1111tk

--- src/main/java/kroryi/bus2/controller/user/UserController.java
+++ src/main/java/kroryi/bus2/controller/user/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 
     @GetMapping("/")
     public String home() {
-        return "redirect:/api/health";
+        return "home";
     }
 
     @GetMapping("/api/health")
